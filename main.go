edf6596ee0cc224b8f73f6b308fdabf73bf73d41
nagaame <sanweishuzu@gmail.com>
2022-05-10 02:11:38 +0800

    add daemon package

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"github.com/takama/daemon"
 	"tw-bot/config"
 	"tw-bot/database"
 	"tw-bot/telegram"
@@ -8,6 +10,17 @@ import (
 )
 
 func main() {
+
+	service, err := daemon.New("tw-bot", "A Telegram bot for Twitter", daemon.UserAgent)
+	if err != nil {
+		panic(err)
+	}
+	status, err := service.Install()
+	if err != nil {
+		panic(err)
+
+	}
+	fmt.Println(status)
 	config.StartConfig()
 	database.LoadData()
 	twitter.Start()
