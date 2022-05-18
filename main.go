5fa169c292e9c22f5dbd028b61b1ca8c2d50de61
nagaame <sanweishuzu@gmail.com>
2022-05-18 16:26:56 +0800

    delete daemon

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"github.com/takama/daemon"
 	"tw-bot/config"
 	"tw-bot/database"
 	"tw-bot/telegram"
@@ -10,17 +8,6 @@ import (
 )
 
 func main() {
-
-	service, err := daemon.New("tw-bot", "A Telegram bot for Twitter", daemon.UserAgent)
-	if err != nil {
-		panic(err)
-	}
-	status, err := service.Install()
-	if err != nil {
-		panic(err)
-
-	}
-	fmt.Println(status)
 	config.StartConfig()
 	database.LoadData()
 	twitter.Start()
